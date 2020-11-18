9dc18d452852fbf3f1bb05511cc86086eea03984
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-11-17 17:52:11 -0700

    notes complete

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,43 +1,43 @@
 """
 You are given a non-empty list of words.
-
+​
 Write a function that returns the *k* most frequent elements.
-
+​
 The list that you return should be sorted by frequency from highest to lowest.
 If two words have the same frequency, then the word with the lower alphabetical
 order should come first.
-
+​
 Example 1:
-
+​
 ```plaintext
 Input:
 words = ["lambda", "school", "rules", "lambda", "school", "rocks"]
 k = 2
-
+​
 Output:
 ["lambda", "school"]
-
+​
 Explanation:
 "lambda" and "school" are the two most frequent words.
 ```
-
+​
 Example 2:
-
+​
 ```plaintext
 Input:
 words = ["the", "sky", "is", "cloudy", "the", "the", "the", "cloudy", "is", "is"]
 k = 4
-
+​
 Output:
 ["the", "is", "cloudy", "sky"]
-
+​
 Explanation:
 "the", "is", "cloudy", and "sky" are the four most frequent words. The words
 are sorted from highest frequency to lowest.
 ```
-
+​
 Notes:
-
+​
 - `k` is always valid: `1 <= k <= number of unique elements.
 - words in the input list only contain lowercase letters.
 ```
@@ -47,9 +47,37 @@ def top_k_frequent(words, k):
     Input:
     words -> List[str]
     k -> int
-
+​
     Output:
     List[str]
     """
     # Your code here
-
+    # 1. figure out how many times each word in `words` shows up 
+    word_counts = {} 
+​
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+​
+    # 2. takes words, starting with the word with highest frequency, until 
+    # we have `k` words
+    # we can use Python's built in sort function to sort the contents of the 
+    # dictionary by the counts 
+    # in the callback, `x` is a key-value pair in the form of a tuple 
+    # res = sorted(word_counts, key=lambda k: (word_counts[k], k))
+    # first sort by the counts, then by the words themselves if their counts are equal
+    res = sorted(word_counts.items(), key=lambda kv: (-kv[1], kv[0]))
+​
+    # grab just the first elements from the tuples in `res`
+    # return [t[0] for t in res[:k]]
+    output = [] 
+​
+    for i in range(k):
+        t = res[i]
+        output.append(t[0])
+​
+    return output
+​
+print(top_k_frequent(["the", "sky", "is", "cloudy", "the", "the", "the", "cloudy", "is", "is"], 4))
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -1,43 +1,43 @@
 """
 Given a string, sort it in decreasing order based on the frequency of characters.
-
+​
 Example 1:
-
+​
 ```plaintext
 Input:
 "free"
-
+​
 Output:
 "eefr"
-
+​
 Explanation:
 'e' appears twice while 'f' and 'r' appear once.
 So 'e' must appear before 'f' and 'r'. Therefore, "eerf" is also a valid answer.
 ```
-
+​
 Example 2:
-
+​
 ```plaintext
 Input:
 "dddbbb"
-
+​
 Output:
 "dddbbb"
-
+​
 Explanation:
 Both 'd' and 'b' appear three times, so "bbbddd" is also a valid answer.
 Note that "dbdbdb" is incorrect, as the same characters must be together.
 ```
-
+​
 Example 3:
-
+​
 ```plaintext
 Input:
 "Bbcc"
-
+​
 Output:
 "ccBb"
-
+​
 Explanation:
 "ccbB" is also a valid answer, but "Bbcc" is incorrect.
 Note that 'B' and 'b' are treated as two different characters.
@@ -47,10 +47,39 @@ def frequency_sort(s: str) -> str:
     """
     Inputs:
     s -> str
-
+​
     Output:
     str
     """
     # Your code here
-
-    
+    # 1. Init and populate a hash table for counts 
+    letter_counts = {}
+​
+    for letter in s:
+        if letter in letter_counts:
+            letter_counts[letter] += 1
+        else:
+            letter_counts[letter] = 1
+​
+    # 2. Sort by the counts in descending order (we don't need to 
+    # worry about the order of letters that have the same count)
+    res = sorted(letter_counts.items(), key=lambda kv: -kv[1])
+​
+    # 3. We need reconstruct the output string from the frequency of each letter 
+    # do this by iterating through `res`, and appending each letter to a list `count`
+    # times 
+​
+    # word = [letter * count for letter, count in res]
+​
+    word = [] 
+​
+    for letter, count in res:
+        word.append(letter * count)
+​
+    # now that the `word` list contains all the necessary characters, join them
+    # into a string
+    # return "".join(letter * count for letter, count in res)
+​
+    return "".join(word)
+​
+print(frequency_sort("Bbcc"))
